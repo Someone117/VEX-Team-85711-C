3b3edc2176a8e58800a98cff0dd07387efe4fc7e
Someone117 <81842426+Someone117@users.noreply.github.com>
2023-01-15 11:16:43 -0700

    Changes to reflect the new robot
    Re added intake, changed indexer to a shaved/slip gear system

--- src/main.cpp
+++ src/main.cpp
@@ -32,40 +32,31 @@ controller::axis upDownAxis() { return Controller1.Axis3; }
 controller::axis leftRightAxis() { return Controller1.Axis4; }
 controller::axis turnAxis() { return Controller1.Axis1; }
 
-void index_forward(){
-  Indexer.spin(vex::forward);
-  this_thread::sleep_for(250);
-  Indexer.stop();
-  this_thread::sleep_for(200);
-}
-
-void index_backward(){
-  Indexer.spin(reverse);
-  this_thread::sleep_for(250);
-  Indexer.stop();
+void index() { // indexes and waits
+  Indexer.startSpinFor(vex::forward, 360, deg);
+  while(!Indexer.isSpinning()) {
+    this_thread::sleep_for(50);
+  }
 }
 
 // we may want to make it not get stuck accedentally
 int indexerTasks() {
-  bool ret = false;
-  bool ret1 = false;
+  bool ret = false; // does the endgame need to return, or trigger
   while(true) {
-    if(Controller1.ButtonR1.pressing() && !ret) {
-      index_backward();
-      ret = true;
-    } else if(ret) {
-      index_forward(); 
-      ret = false;
+    if(Controller1.ButtonR1.pressing() && !Indexer.isSpinning()) { // If the indexer finished, start it spinning
+      Indexer.startSpinFor(vex::forward, 360, deg);
     }
     if(Controller1.ButtonDown.pressing()) {
-      if(ret1) {
-        //TODO: CKECK AND MAKE BETTER ENDGAME
-        EndGame.spinFor(vex::reverse, 130, degrees);
-        ret1 = false;
-      } else {
-        EndGame.spinFor(vex::forward, 130, degrees);
-        ret1 = true;
+      if(!EndGame.isSpinning()) {
+        if(ret) {
+          EndGame.startSpinFor(vex::reverse, 130, degrees);
+          ret = false;
+        } else {
+          EndGame.startSpinFor(vex::forward, 130, degrees);
+          ret = true;
+        }
       }
+      
     }
     this_thread::sleep_for(50);
   }
@@ -73,36 +64,34 @@ int indexerTasks() {
 }
 
 int flywheelTasks() {
-  bool fly = true;
-  bool fly1 = false;
+  bool fly = true; //should flywheel be spinning
+  bool flyWheelSpinState = false; //which velocity should it be spinning at
   while(true) {
     if(Controller1.ButtonUp.pressing()) {
       if(fly) {
         Flywheel.stop();
         fly = false;
-        this_thread::sleep_for(100);
       } else {
         Flywheel.spin(vex::forward);
         fly = true;
-        this_thread::sleep_for(100);
       }
     } else if(Controller1.ButtonA.pressing()) {
-      if(fly1) {
+      if(flyWheelSpinState) {
         Flywheel.setVelocity(475, rpm);
       } else {
         Flywheel.setVelocity(415, rpm);
       }
-      
     }
+    this_thread::sleep_for(100);
   }
   return 1;
 }
 
-void run(bool flipDrive) {
-  double deadZone = 15;
-  Comp_Vector driveVec(abs(upDownAxis().position()) > deadZone ? upDownAxis().position() : 0, abs(leftRightAxis().position()) > deadZone ? -leftRightAxis().position() : 0);
+void run(bool flipDrive) { // drive and intake
+  double deadZone = 15; //prevents drifting
+  Comp_Vector driveVec(abs(upDownAxis().position()) > deadZone ? upDownAxis().position() : 0, abs(leftRightAxis().position()) > deadZone ? -leftRightAxis().position() : 0); //snap to 0 if any individual axis is in the deadzone
   double turnAmt = turnAxis().position();
-  if(abs(turnAmt) > deadZone || driveVec.get_mag() > deadZone) {
+  if(abs(turnAmt) > deadZone || driveVec.get_mag() > deadZone) { // are we outside the deadzone
     if(driveVec.get_mag() > abs(turnAmt)) {
       drive(driveVec.get_mag() > deadZone ? driveVec : Comp_Vector(0,0), flipDrive);
     } else {
@@ -111,24 +100,24 @@ void run(bool flipDrive) {
   } else {
     stopDrive();
   }
-  if(Controller1.ButtonL2.pressing()) {
-    //Intake.setVelocity(100, percent);
-    //Intake.spin(vex::forward);
-  } else if(Controller1.ButtonR2.pressing()) {
-    //Intake.setVelocity(100, percent);
-    //Intake.spin(vex::reverse);
-  } //else 
-  if(Controller1.ButtonL2.pressing()) {
+
+
+  if(Controller1.ButtonL2.pressing()) { // spin intake in
+    Intake.setVelocity(100, percent);
+    Intake.spin(vex::forward);
+  } else if(Controller1.ButtonR2.pressing()) { // spin intake out
+    Intake.setVelocity(100, percent);
+    Intake.spin(vex::reverse);
+  } else if(Controller1.ButtonL2.pressing()) { // spin rollers
     Intake.setVelocity(70, percent);
     Intake.spin(vex::reverse);
   } else {
     Intake.stop();
   }
-  
   this_thread::sleep_for(50);
 }
 
-void init() {
+void init() { // init all torque, velocity and breaking
   FrontLeft.setMaxTorque(100,percent);
   FrontLeft.setStopping(brake);
   FrontRight.setMaxTorque(100,percent);
@@ -148,16 +137,15 @@ void init() {
 
   EndGame.setVelocity(100, percent);
 
-
-  stopDrive();
+  stopDrive(); //safety
 }
 
 void teleop() {
   init();
-  Flywheel.spin(vex::forward);
+  Flywheel.spin(vex::forward); //start flywheel
   stopDrive();
-  thread indexer_thread(indexerTasks);
-  bool flip = false;
+  thread indexer_thread(indexerTasks); // enable the indexer
+  bool flip = false; //should we reverse the drive
   while(true) {
     run(flip);
     if(Controller1.ButtonX.pressing()) {
@@ -194,10 +182,10 @@ void auton_left() {
   a.set_mag(a.get_mag()*-1);
   drive(a, false);
   this_thread::sleep_for(500);
-  for(int i = 0; i < 2; i++){
-    index_forward();
-    index_backward();
-  }
+  // shoot three times, but we will have 2 disks
+  index();
+  index();
+  index();
   Flywheel.stop();
   
   // should uno reverse and drive into the other roller backwards
@@ -227,15 +215,14 @@ void auton_right() {
   Intake.spin(vex::reverse, 70, percent);
   this_thread::sleep_for(250);
   Intake.stop();
-  // shoot twice
-  for(int i = 0; i < 2; i++){
-    index_forward();
-    index_backward();
-  }
+  // shoot three times, but we will have 2 disks
+  index();
+  index();
+  index();
   Flywheel.stop();
 }
 
-void auton(autons aut) {
+void auton(autons aut) { //choose which auton
   switch(aut) {
     case L: auton_left();
     case R: auton_right();
