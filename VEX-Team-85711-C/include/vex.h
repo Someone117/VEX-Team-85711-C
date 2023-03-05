60a59142ce8e69454362bb0fa23f9bec9e890116
notan79 <112504114+notan79@users.noreply.github.com>
2023-03-05 11:50:04 -0700

    Added PID basics
    updated some errors with the previous upload and added the PID system for movement

---
+++ VEX-Team-85711-C/include/vex.h
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       vex.h                                                     */
+/*    Author:       Vex Team 85711C */
+/*    Created:      1 Feb 2019                                                */
+/*    Description:  Default header for V5 projects                            */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+//
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "v5.h"
+#include "v5_vcs.h"
+
+#include "robot-config.h"
+
+#define waitUntil(condition)                                                   \
+  do {                                                                         \
+    this_thread::sleep(5);                                                     \
+  } while (!(condition))
+
+#define repeat(iterations)                                                     \
+  for (int iterator = 0; iterator < iterations; iterator++)
