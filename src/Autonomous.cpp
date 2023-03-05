cbd4f2db13595cb6e61284b54408570efaef0386
notan79 <112504114+notan79@users.noreply.github.com>
2023-03-05 11:58:58 -0700

    reupload of new pid

--- src/Autonomous.cpp
+++ src/Autonomous.cpp
@@ -1,13 +1,8 @@
 #include "autonomous.h"
 #include "tankDrive.h"
 #include "vex.h"
-#include "robot-config.cpp"
-
-float wheel_diameter = 3.25f;
-motor_group parent = LeftDrive;
-motor_group child = RightDrive; 
-int encoder_ticks = 300;
-const float PI = 3.1415927f;
+#include "robot-config.h"
+#include "Main_PID.h"
 
 void auton_skills() {
   drive(-75, false);
@@ -110,61 +105,75 @@ void auton_right() {
 void auton_left_simple() {}
 
 
-
+double volts_checked(double volts, double max){
+  if(volts > max){
+    return max;
+  }
+  if(volts < -max){
+    return -max;
+  }
+  return volts; 
+}
 
 
 // this function finds a similar speed for the motorgroups
-void auto_drive(int speed, double c){
-  float diff = fabs(parent.rotation(rev)) - fabs(child.rotation(rev));
+void auto_drive(double volts, double MAX_V, double c){
+  double diff = std::abs(LeftDrive.rotation(rotationUnits::rev)) - std::abs(RightDrive.rotation(rotationUnits::rev));
   int sign = diff < 0 ? -1 : 1; 
-  int dV = sign*(diff)*speed*c; // c is a constant that needs tuning
+  double dV = sign*(diff)*c; // c is a constant that needs tuning
 
-  parent.spin(forward, speed, rpm);
-  child.spin(forward, speed + dV, rpm);
+  LeftDrive.spin(forward, volts, voltageUnits::volt);
+  RightDrive.spin(forward, volts_checked(volts + dV, MAX_V), voltageUnits::volt);
 }
 
-void auto_drive_dist(int speed, int dist){
+void auto_drive_dist(int dist){
   // reseting the rotation vals to 0
-  parent.resetRotation();
-  child.resetRotation();
+  LeftDrive.resetRotation();
+  RightDrive.resetRotation();
+  const double wheel_diameter = 3.25;
+  const double MAX_V = 5;
 
-  int revolutions = dist/(wheel_diameter*PI); // converts inches into revolutions
-  double gear_ratio = 1; 
-  // loops to slow down as the bot approaches its end position
-  while(fabs(parent.rotation(rev)*gear_ratio)<revolutions*0.6){
-    auto_drive(speed, 0.1);
-  }
-  while(fabs(parent.rotation(rev)*gear_ratio)<revolutions){
-    auto_drive(speed/3, 0.1);
-  }
+  double total_revs = dist/(wheel_diameter*M_PI); // converts inches into revolutions
+  double gear_ratio = 2/3; 
+  double volts;
 
-  // this counteracts the momentum and stops the bot
-  auto_drive(-speed/2, 1);
-  this_thread::sleep_for(50);
-  auto_drive(0, 1); 
+  // PID
+  Main_PID pid(total_revs); 
+  while(LeftDrive.rotation(rotationUnits::rev)*gear_ratio < total_revs){
+    pid.set_e_cur(total_revs - std::abs(LeftDrive.rotation(rotationUnits::rev)*gear_ratio) ); // total_revs - cur_revs
+    pid.sum_int();
+    pid.find_deriv();
+    volts = volts_checked(pid.output(), MAX_V);
+    auto_drive(volts, MAX_V, 0.1);
+
+    this_thread::sleep_for(50);
+  }
+ 
+  auto_drive(0, MAX_V, 1); 
 }
 
-void auto_turn(int speed){
-  parent.spin(forward, speed, rpm);
-  child.spin(forward, -speed, rpm);
+void auto_turn(double volts){
+  LeftDrive.spin(forward, volts, voltageUnits::volt);
+  RightDrive.spin(forward, -volts, voltageUnits::volt);
 }
 
-void auto_turn_deg(int speed, int deg){
-  float r = 0.2f; //this needs to be tuned --> it is basically motor_rev/bot_degree_turned
+void auto_turn_deg(double volts, int deg){
+  double r = 0.2; //this needs to be tuned
+  volts = volts_checked(volts, 5); 
 
-  parent.resetRotation();
-  child.resetRotation();
+  LeftDrive.resetRotation();
+  RightDrive.resetRotation();
 
   //slows down the bot as it approaches it makes rev
   //also gear_ratio doesn't matter because r accounts for it I think
-  while(fabs(parent.rotation(rev))/r < deg*0.8){
-    auto_turn(speed);
+  while(std::abs(LeftDrive.rotation(rotationUnits::rev))/r < deg*0.8){
+    auto_turn(volts);
   }
-  while(fabs(parent.rotation(rev))/r < deg*0.8){
-    auto_turn(speed/3);
+  while(std::abs(LeftDrive.rotation(rotationUnits::rev))/r < deg*0.8){
+    auto_turn(volts/3);
   }
   
-  auto_turn(-speed/2);
+  auto_turn(-volts/2);
   this_thread::sleep_for(50);
   auto_turn(0); 
 }
@@ -174,15 +183,16 @@ To use this function, enter the speed, in rpms, and a r_test.
 Decide on a degree amount you want the bot to turn: maybe 90 degrees.
 Keep changing r_test until the bot actually reaches that 90 degrees. 
 */
-void tune_k_for_turn(int speed, int r_test){
-  parent.resetRotation();
-  child.resetRotation();
+void tune_r_for_turn(double volts, int r_test){
+  volts = volts_checked(volts, 5); 
+  LeftDrive.resetRotation();
+  RightDrive.resetRotation();
 
-  while(fabs(parent.rotation(rev)) < r_test){
-    auto_turn(speed);
+  while(std::abs(LeftDrive.rotation(rotationUnits::rev)) < r_test){
+    auto_turn(volts);
   }
 
-  auto_turn(-speed/2);
+  auto_turn(-volts/2);
   this_thread::sleep_for(50);
   auto_turn(0); 
 }
